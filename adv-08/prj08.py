db1cd3cb390e5ca2c72177af1701e2fb639ac2d3
Charles-Monster <charlesweng1011@gmail.com>
2024-04-26 21:07:59 +0800

    prj08+hw

--- adv-08/prj08.py
+++ adv-08/prj08.py
@@ -12,19 +12,24 @@ import os
 from pygame.locals import *
 #######################初始化#######################
 os.chdir(sys.path[0])
-pygame.init
+pygame.init()
 LIMIT_LOW=140
 clock=pygame.time.Clock()
+red=(255,0,0)
 ######################載入圖片物件##################
 img=pygame.image.load('image/bg.png')
 img_dinosaur=[
     pygame.image.load('image/小恐龍1.png'),
     pygame.image.load('image/小恐龍2.png'),
 ]
-img_cati=pygame.image.load('image/cacti.png')
+img_cacti=pygame.image.load('image/cacti.png')
 bg_x=img.get_width()
 bg_y=img.get_height()
 bg_roll_x=0
+#####################分數物件#######################
+score=0
+typeface=pygame.font.get_default_font()
+score_font=pygame.font.Font(typeface,36)
 #####################恐龍物件#######################
 ds_x=50
 ds_y=LIMIT_LOW
@@ -32,10 +37,16 @@ ds_index=0
 jumpstate=False
 jumpvalue=0
 jumpheight=13
+ds_center_x=ds_x+img_dinosaur[0].get_width()/2
+ds_center_y=ds_y+img_dinosaur[0].get_height()/2
+ds_detect_r=min(img_dinosaur[0].get_width(),img_dinosaur[0].get_height())/2
 #####################仙人掌物件#######################
-catci_x=bg_x-100
-catci_y=LIMIT_LOW
-catci_shift=10
+cacti_x=bg_x-100
+cacti_y=LIMIT_LOW
+cacti_shift=10
+cacti_center_x=cacti_x+img_dinosaur[0].get_width()/2
+cacti_center_y=cacti_y+img_dinosaur[0].get_height()/2
+cacti_detect_r=max(img_cacti[0].get_width(),img_cacti[0].get_height())/2-15
 ######################建立視窗######################
 screen=pygame.display.set_mode([bg_x,bg_y])
 pygame.display.set_caption('Dinosaur')
@@ -60,12 +71,23 @@ def move_dinosaur():
             jumpstate=False
             ds_y=LIMIT_LOW
     ds_index=(ds_index-1)% len(img_dinosaur)
+    ds_center_x=ds_x+img_dinosaur[ds_index].get_width()/2
+    ds_center_y=ds_y+img_dinosaur[ds_index].get_height()/2
     screen.blit(img_dinosaur[ds_index],(ds_x,ds_y))
 def move_cacti():
-    global catci_x
-    catci_x=(catci_x-catci_shift)%(bg_x-100)
-    screen.blit(img_cati,(catci_x,catci_y))
-
+    global cacti_x,score
+    cacti_x=(cacti_x-cacti_shift)%(bg_x-100)
+    screen.blit(img_cacti,(cacti_x,cacti_y))
+    if cacti_x<=0:
+        score +=1
+def score_update():
+    score_sur=score_font.render(str(score),True,red)
+    screen.blit(score_sur,[10,10])
+def is_hit(x1,y1,x2,y2,r):
+    if ((x1-x2)**2+(y1-y2)**2)<(r*r):
+        return True
+    else:
+        return False
 ######################循環偵測######################
 while True:
     clock.tick(20)
@@ -78,5 +100,6 @@ while True:
                 jumpstate=True
     bg_update()
     move_dinosaur()
+    score_update()
     move_cacti()
     pygame.display.update()
